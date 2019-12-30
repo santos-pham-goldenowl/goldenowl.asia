ad6a884b6ad040d6edec838a2e893a6206bad591
Thanh Truong <thanhtruong1216@gmail.com>
2019-12-30 10:44:49 +0700

    load fonts and images

---
+++ src/utils/PartnersLogo.js
@@ -0,0 +1,35 @@
+import assred from '../images/partner_logos/1.png';
+import finda from '../images/partner_logos/2.png';
+import luxe from '../images/partner_logos/3.png';
+import candy from '../images/partner_logos/4.png';
+import gig from '../images/partner_logos/5.png';
+import mywork from '../images/partner_logos/6.png';
+
+const partnersLogo = [
+  {
+    name: 'assured',
+    url: assred
+  },
+  {
+    name: 'finda',
+    url: finda
+  },
+  {
+    name: 'luxe',
+    url: luxe
+  },
+  {
+    name: 'candy',
+    url: candy
+  },
+  {
+    name: 'gig',
+    url: gig
+  },
+  {
+    name: 'assured',
+    url: mywork
+  },
+]
+
+export default partnersLogo;
---
+++ src/utils/TechnologiesLogo.js
@@ -0,0 +1,125 @@
+import a1 from '../images/tech_logos/1.png';
+import a2 from '../images/tech_logos/2.png';
+import a3 from '../images/tech_logos/3.png';
+import a4 from '../images/tech_logos/4.png';
+import a5 from '../images/tech_logos/5.png';
+import a6 from '../images/tech_logos/6.png';
+import a7 from '../images/tech_logos/7.png';
+import a8 from '../images/tech_logos/8.png';
+import a9 from '../images/tech_logos/9.png';
+import a10 from '../images/tech_logos/10.png';
+import a11 from '../images/tech_logos/11.png';
+import a12 from '../images/tech_logos/12.png';
+import a13 from '../images/tech_logos/13.png';
+import a14 from '../images/tech_logos/14.png';
+import a15 from '../images/tech_logos/15.png';
+import a16 from '../images/tech_logos/16.png';
+import a17 from '../images/tech_logos/17.png';
+import a18 from '../images/tech_logos/18.png';
+import a19 from '../images/tech_logos/19.png';
+import a20 from '../images/tech_logos/20.png';
+import a21 from '../images/tech_logos/21.png';
+import a22 from '../images/tech_logos/22.png';
+import a23 from '../images/tech_logos/23.png';
+import a24 from '../images/tech_logos/24.png';
+
+const technologiesLogo = [
+  {
+    name: 'a1',
+    url: a1
+  },
+  {
+    name: 'a2',
+    url: a2
+  },
+  {
+    name: 'a3',
+    url: a3
+  },
+  {
+    name: 'a4',
+    url: a4
+  },
+  {
+    name: 'a5',
+    url: a5
+  },
+  {
+    name: 'a6',
+    url: a6
+  },
+  {
+    name: 'a7',
+    url: a7
+  },
+  {
+    name: 'a8',
+    url: a8
+  },
+  {
+    name: 'a9',
+    url: a9
+  },
+  {
+    name: 'a10',
+    url: a10
+  },
+  {
+    name: 'a11',
+    url: a11
+  },
+  {
+    name: 'a12',
+    url: a12
+  },
+  {
+    name: 'a13',
+    url: a13
+  },
+  {
+    name: 'a14',
+    url: a14
+  },
+  {
+    name: 'a15',
+    url: a15
+  },
+  {
+    name: 'a16',
+    url: a16
+  },
+  {
+    name: 'a17',
+    url: a17
+  },
+  {
+    name: 'a18',
+    url: a18
+  },
+  {
+    name: 'a19',
+    url: a19
+  },
+  {
+    name: 'a20',
+    url: a20
+  },
+  {
+    name: 'a21',
+    url: a21
+  },
+  {
+    name: 'a22',
+    url: a22
+  },
+  {
+    name: 'a23',
+    url: a23
+  },
+  {
+    name: 'a24',
+    url: a24
+  },
+]
+
+export default technologiesLogo;
