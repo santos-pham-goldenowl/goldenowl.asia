f577be912710daf5e59cf3fff663c7d9138a8051
Thanh Truong <thanhtruong1216@gmail.com>
2019-12-30 14:04:13 +0700

    config eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "env": {
+    "node": true,
+    "browser": true,
+    "es6": true
+  },
+  "extends": "airbnb",
+  "parser": "babel-eslint",
+  "rules": {
+    "no-console": "off",
+    "no-debugger": "off",
+    "global-require": "off",
+    "no-restricted-globals": "off",
+    "react/jsx-filename-extension": [
+      "warn",
+      {
+        "extensions": [".js", ".jsx"]
+      }
+    ],
+    "max-len": ["error", 120, { "ignoreTemplateLiterals": true }],
+    "no-unused-expressions": ["error", { "allowShortCircuit": true }]
+  }
+}
