4781d89fc2fa9fb2a9516ab96fe7fdd19d3d7e93
Thanh Truong <thanhtruong1216@gmail.com>
2019-12-31 16:07:48 +0700

    create footer

---
+++ src/utils/SocialIcons.js
@@ -0,0 +1,20 @@
+import facebook from '../assets/images/facebook-f-brands.svg';
+import instagram from '../assets/images/instagram-brands.svg';
+import linkedin from '../assets/images/linkedin-in-brands.svg';
+
+const socialIcons = [
+  {
+    name: facebook,
+    url: facebook
+  },
+  {
+    name: instagram,
+    url: instagram
+  },
+  {
+    name: linkedin,
+    url: linkedin
+  }
+]
+
+export default socialIcons;
