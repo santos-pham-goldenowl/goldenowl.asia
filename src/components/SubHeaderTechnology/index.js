316720f7ef89e86eca2c106ad0d6627197842d2b
Thanh Truong <thanhtruong1216@gmail.com>
2020-01-02 21:00:38 +0700

    refactor - remove technology content

--- src/components/SubHeaderTechnology/index.js
+++ src/components/SubHeaderTechnology/index.js
@@ -3,36 +3,36 @@ import './index.sass';
 import arrowDown from '../../assets/images/chevron-down-solid.svg';
 
 class Technology extends Component {
-  state = {
-    showTechnologies: false
-  }
+  // state = {
+  //   showTechnologies: false
+  // }
 
-  handleTechnologies = () => {
-    this.setState({
-      showTechnologies: !this.state.showTechnologies
-    });
-  }
+  // handleTechnologies = () => {
+  //   this.setState({
+  //     showTechnologies: !this.state.showTechnologies
+  //   });
+  // }
   render() {
-    if (this.state.showTechnologies) {
-      return (
-        <section>
-          <div className="technology__arrow-wrapper" onClick={this.handleTechnologies}>
-            <h4>Technology</h4>
-            <img className="arrow-down" src={arrowDown} alt="arrow-down" />
-          </div>
-          <div className="technology">
-            <p>Technology</p>
-          </div>
-        </section>
-      );
-    } else {
-      return (
-        <div className="technology__arrow-wrapper" onClick={this.handleTechnologies}>
-          <h4>Technology</h4>
-          <img className="arrow-down" src={arrowDown} alt="arrow-down" />
-        </div>
-      )
-    }
+    // if (this.state.showTechnologies) {
+    //   return (
+    //     <section>
+    //       <div className="technology__arrow-wrapper" onClick={this.handleTechnologies}>
+    //         <h4>Technology</h4>
+    //         <img className="arrow-down" src={arrowDown} alt="arrow-down" />
+    //       </div>
+    //       <div className="technology">
+    //         <p>Technology</p>
+    //       </div>
+    //     </section>
+    //   );
+    // } else {
+    return (
+      <div className="technology__arrow-wrapper" onClick={this.handleTechnologies}>
+        <h4>Technology</h4>
+        <img className="arrow-down" src={arrowDown} alt="arrow-down" />
+      </div>
+    )
+    // }
   }
 }
 export default Technology;
