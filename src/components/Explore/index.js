0cf56a4353a9b04a54d7fd14a187ff5809821abe
Thanh Truong <thanhtruong1216@gmail.com>
2020-01-03 10:27:09 +0700

    style - change padding and background

--- src/components/Explore/index.js
+++ src/components/Explore/index.js
@@ -6,15 +6,19 @@ import './index.sass';
 function Explore() {
   return (
     <section className="explore">
-      <div className="explore__top-content">
-        <h6>BID4SKILL | FEATURES CASE STUDY</h6>
-        <div className="explore__case-study">
-          <h6>VIEW ALL CASE STUDY</h6>
-          <ArrowRight />
+      <div>
+        <div className="explore__top-content">
+          <h6>BID4SKILL | FEATURES CASE STUDY</h6>
+          <div className="explore__case-study">
+            <h6>VIEW ALL CASE STUDY</h6>
+            <ArrowRight />
+          </div>
+        </div>
+        <p className="explore__description">Lorem ipsum dolor sit amet, iudico oratio eos ex. Duo ludu.</p>
+        <div className="explore__image-wrapper">
+          <img className="explore__image" src={laptop} alt="computer" />
         </div>
       </div>
-      <p className="explore__description">Lorem ipsum dolor sit amet, iudico oratio eos ex. Duo ludu.</p>
-      <img className="explore__image" src={laptop} alt="computer" />
     </section>
   );
 }
