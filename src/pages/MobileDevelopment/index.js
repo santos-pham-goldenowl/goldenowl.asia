f8193ae90e1a67f058a61b5ff2dbfb98a3de1750
Thanh Truong <thanhtruong1216@gmail.com>
2020-01-09 17:35:22 +0700

    feat - create service page

---
+++ src/pages/MobileDevelopment/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Service from '../../components/Service/index';
+import MobileDevelopmentData from '../../utils/MobileDevelopment';
+import coverImage from '../../assets/images/service_logos/MobileDevelopment/cover.svg';
+import dataOrdered from '../../utils/OtherServices';
+
+const MobileDevelopment = () => (
+  <Service
+    coverImage={coverImage}
+    serviceData={MobileDevelopmentData}
+    pageName="Services/Mobile Development"
+    otherServiceImages={dataOrdered.mobileDevelopment}
+  />
+);
+
+export default MobileDevelopment;
---
+++ src/pages/ProjectManagement/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Service from '../../components/Service/index';
+import ProjectManagementData from '../../utils/ProjectManagement';
+import coverImage from '../../assets/images/service_logos/ProjectManagement/cover.svg';
+import dataOrdered from '../../utils/OtherServices';
+
+const ProjectManagementService = () => (
+  <Service
+    coverImage={coverImage}
+    serviceData={ProjectManagementData}
+    pageName="Services/Project Management"
+    otherServiceImages={dataOrdered.projectManagement}
+  />
+);
+
+export default ProjectManagementService;
