296c2fd34f9014868822b9613ed3321a28a91685
Thanh Truong <thanhtruong1216@gmail.com>
2020-01-10 14:34:48 +0700

    fix - refactor state

--- src/components/Tabs/TabHeader.js
+++ src/components/Tabs/TabHeader.js
@@ -2,26 +2,15 @@ import React from 'react';
 
 class TabHeader extends React.Component {
 
-  state = {
-    headerActive: false
-  }
-
   onClick() {
     if(this.props.onSelect) {
       this.props.onSelect(this.props.for);
-      this.setState({
-        headerActive: true
-      })
-    } else {
-      this.setState({
-        headerActive: false
-      })
     }
   }
 
   render() {
     return (
-      <div className={`header header-active-${this.state.headerActive}`} onClick={this.onClick.bind(this)}>{this.props.children}
+      <div className="header" onClick={this.onClick.bind(this)}>{this.props.children}
       </div>
     );
   }
