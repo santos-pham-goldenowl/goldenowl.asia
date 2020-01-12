eeb68ecc8afd4bc55e8961af230a6482306a9686
Thanh Truong <thanhtruong1216@gmail.com>
2020-01-12 17:53:48 +0700

    fix - dropdown menu

--- src/components/SubHeaderServices/index.js
+++ src/components/SubHeaderServices/index.js
@@ -9,50 +9,62 @@ class Services extends Component {
     showServices: false
   }
 
-  handleServices = () => {
+  handleServicesOn = () => (
     this.setState({
-      showServices: !this.state.showServices
-    });
-  }
+      showServices: true
+    })
+  );
+
+  handleServicesOff = () => (
+    this.setState({
+      showServices: false
+    })
+  );
+
   render() {
-    if (this.state.showServices) {
-      return (
-        <section>
-          <div className="services__arrow-wrapper" onMouseEnter={this.handleServices}>
-            <Link to="/services">
-              <h4 className="sub-header__text">Services</h4>
-            </Link>
-            <img className="arrow-down" src={arrowDown} alt="arrow-down" />
-          </div>
-          <div className="services">
-            {ServicesData.map(service => {
-              return (
-                <Link to={`/services/${service.link}`}>
-                  <div className="services__service">
-                    <div className="services__icon-wrapper">
-                      <img src={service.url} alt={service.name} />
-                    </div>
-                    <div className="services__service-description-wrapper">
-                      <h4>{service.name}</h4>
-                      <p className="services__service-description">{service.description}</p>
-                    </div>
-                  </div>
-                </Link>
-              );
-            })}
-          </div>
-        </section>
-      );
-    } else {
-      return (
-        <div className="services__arrow-wrapper" onMouseLeave={this.handleServices}>
+    const servicesNode = (
+      <section className="services">
+        <div className="services__arrow-wrapper" onMouseEnter={this.handleServicesOn}>
           <Link to="/services">
             <h4 className="sub-header__text">Services</h4>
           </Link>
           <img className="arrow-down" src={arrowDown} alt="arrow-down" />
         </div>
-      )
-    }
+        <div className="services__items" onMouseLeave={this.handleServicesOff}>
+          {ServicesData.map(service => {
+            return (
+              <Link to={`/services/${service.link}`}>
+                <div className="services__service">
+                  <div className="services__icon-wrapper">
+                    <img src={service.url} alt={service.name} />
+                  </div>
+                  <div className="services__service-description-wrapper">
+                    <h4>{service.name}</h4>
+                    <p className="services__service-description">{service.description}</p>
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+      </section>
+    );
+
+    const buttoNode = (
+      <section className="services">
+        <div className="services__arrow-wrapper" onMouseEnter={this.handleServicesOn}>
+          <Link to="/services">
+            <h4 className="sub-header__text">Services</h4>
+          </Link>
+          <img className="arrow-down" src={arrowDown} alt="arrow-down" />
+        </div>
+      </section>
+    );
+    return(
+      <div>
+        {this.state.showServices ? servicesNode : buttoNode}
+      </div>
+    )
   }
 }
 export default Services;
