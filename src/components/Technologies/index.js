8a063b12ecca49b9cd8c38d210d10c0604ca58fc
Thanh Truong <thanhtruong1216@gmail.com>
2020-01-12 22:14:41 +0700

    style - change grid gap

--- src/components/Technologies/index.js
+++ src/components/Technologies/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import technologiesLogo from '../../utils/TechnologiesLogo';
 import './index.sass';
 
-function Technologies() {
-  return (
-    <section className="technologies">
-      <h2 className="technologies__title">Technologies we use</h2>
-      <div className="technologies__grid">
-        {technologiesLogo.map((logo, index) => <img key={index} src={logo.url} alt={logo.name} />)}
-      </div>
-      <span className="technologies__view-all">View all technologies</span>
+const Technologies = () => (
+  <section className="technologies">
+    <h2 className="technologies__title">Technologies we use</h2>
+    <div className="technologies__grid">
+      {technologiesLogo.map((logo, index) => <img className={`technologies__${logo.name}`} key={index} src={logo.url} alt={logo.name} />)}
+    </div>
+    <span className="technologies__view-all">View all technologies</span>
   </section>
-  );
-}
+);
 
 export default Technologies;
