010ee7695599681a360eeda00a2632e9a821716e
Thanh Truong <thanhtruong1216@gmail.com>
2020-01-13 20:57:11 +0700

    feat - active tab

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -3,12 +3,9 @@ import TabHeader from './TabHeader';
 import TabContent from './TabContent';
 
 class Tabs extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      selected: 'tab1',
-    };
-  }
+  state = {
+    selected: '',
+  };
 
   toogleTab(name) {
     this.setState({ selected: name });
@@ -16,14 +13,18 @@ class Tabs extends React.Component {
 
   render() {
     const {children} = this.props;
-    const {selected} = this.state;
-    const tabHeaders = children.filter(c => c.type === TabHeader).map(c => (
-      React.cloneElement(c, {onSelect: this.toogleTab.bind(this)})
+    let {selected} = this.state;
+
+    selected = selected || children.filter(c => c.type === TabHeader)[0].props.for;
+
+    const tabHeaders = children.filter(c => c.type == TabHeader).map((c) => (
+      React.cloneElement(c, {onSelect: this.toogleTab.bind(this), selected})
     ));
 
     const tabContents = children.filter( c => c.type === TabContent).filter(c => (
       c.props.name === selected
     ));
+
     return (
       <div>
         <div className="header-group">
