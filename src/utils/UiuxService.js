0e0dcf32bbc3450a85196647e3678660601f8197
Thanh Truong <thanhtruong1216@gmail.com>
2020-01-17 14:41:02 +0700

    feat - update pages: project management, web development, ui ux design

--- src/utils/UiuxService.js
+++ src/utils/UiuxService.js
@@ -4,27 +4,47 @@ import ps from '../assets/images/service_logos/UiUx/ps.svg';
 import sketch from '../assets/images/service_logos/UiUx/sketch.svg';
 import xd from '../assets/images/service_logos/UiUx/xd.svg';
 
-const UiUxData = [
-  {
-    name: 'sketch',
-    url: sketch
+const UiUxData = {
+  pageTitle: 'UX UI design',
+  subTitle: 'We offer a design services.',
+  intro: 'A good user experience is the best glue between a brand and (potential) customers. We ensure a consistent brand experience in all interactions with the user, from the look & feel of the various platforms, to the content and the communication between brand and customer.',
+  subIntro: 'All our team members here at shape understand web design and application design. We can create UX UI design services in specific areas to help your business. Creating unique visuals to make you and your brand stand out from the crowd, and more importantly - your competitors.',
+  slogan: {
+    title: 'Simple and powerful',
+    leftContent: {
+      title: 'Ideation',
+      content: 'We design, make prototypes and test design concepts to ensure that users achieve their goals smoothly.'
+    },
+    centerContent: {
+      title: 'Usability research',
+      content: 'Through usability research we learn how visitors experience the platform and we make recommendations for optimisation.'
+    },
+    rightContent: {
+      title: 'Interaction design',
+      content: 'We design interactions between users and products. The most important thing is that the user achieves his goal as simply as possible, something we test on the basis of detailed prototypes.'
+    }
   },
-  {
-    name: 'ps',
-    url: ps
+  achieves: {
+    title: 'Seamless Integration with popular design tools.',
+    description: 'Lorem ipsum dolor sit amet, at per habeo solet, vidisse graecis periculis eu mei. Ea illum ullamcorper mei. Eos vocibus fabellas tacimates cu, at usu eros discere dissentias. Sea cu oratio aeterno alterum, dicant corpora id vis.'
   },
-  {
-    name: 'ai',
-    url: ai
-  },
-  {
-    name: 'xd',
-    url: xd
-  },
-  {
-    name: 'in-vision',
-    url: inVision
-  },
-];
-
+  logos: [
+    {
+      name: 'sketch',
+      url: sketch,
+    },
+    {
+      name: 'ps',
+      url: ps
+    },
+    {
+      name: 'ai',
+      url: ai
+    },
+    {
+      name: 'inVision',
+      url: inVision
+    },
+  ]
+}
 export default UiUxData;
--- src/utils/WebDevelopment.js
+++ src/utils/WebDevelopment.js
@@ -1,40 +1,69 @@
 import html from '../assets/images/service_logos/WebDevelopment/html.svg';
 import css3 from '../assets/images/service_logos/WebDevelopment/css3.svg';
 import js from '../assets/images/service_logos/WebDevelopment/js.svg';
+import react from '../assets/images/service_logos/WebDevelopment/react.svg';
+import angular from '../assets/images/service_logos/WebDevelopment/angular.svg';
+import mock from '../assets/images/service_logos/WebDevelopment/mock.svg';
+import plus from '../assets/images/service_logos/WebDevelopment/50plus.svg';
 import amazon from '../assets/images/service_logos/WebDevelopment/amazon.svg';
 import objectC from '../assets/images/service_logos/WebDevelopment/object-C.svg';
 import arrow from '../assets/images/service_logos/WebDevelopment/arrow.svg';
 import php from '../assets/images/service_logos/WebDevelopment/php.svg';
 
-const WebDevelopmentData = [
-  {
-    name: 'html',
-    url: html
+const WebDevelopmentData = {
+  pageTitle: 'Web development',
+  subTitle: 'What can we bring',
+  intro: 'We - Golden Owl is a professional company with great experience in web development from small to big web project in many from many fields such as business, finance, management, etc.',
+  subIntro: 'As a professional company, we are able to work on many programming languages for web development. We always update the latest technologies so that we can serve the customer the best solutions.',
+  slogan: {
+    title: 'Simple and powerful',
+    leftContent: {
+      title: 'Quality',
+      content: 'We have young and active people who are led and instructed by senior developers. Also, we understand technologies so we know how to produce the good and clean code.'
+    },
+    centerContent: {
+      title: 'Productivity',
+      content: 'Beside owning an expert and experienced developer team, we also have good project managers who have good background in management. We know how to control the time and tasks to make it delivery in time.'
+    },
+    rightContent: {
+      title: 'Security',
+      content: 'Beside the quality of the code, security is set as importance as well. With experience in deploying many websites, we know how to make your web site is protected from being attacked by bad guys.'
+    }
   },
-  {
-    name: 'css3',
-    url: css3
+  achieves: {
+    title: 'Seamless Integration with 50+ software platforms.',
+    description: 'Lorem ipsum dolor sit amet, at per habeo solet, vidisse graecis periculis eu mei. Ea illum ullamcorper mei. Eos vocibus fabellas tacimates cu, at usu eros discere dissentias. Sea cu oratio aeterno alterum, dicant corpora id vis.'
   },
-  {
-    name: 'js',
-    url: js
-  },
-  {
-    name: 'amazone',
-    url: amazon
-  },
-  {
-    name: 'object-C',
-    url: objectC
-  },
-  {
-    name: 'rocket',
-    url: arrow
-  },
-  {
-    name: 'php',
-    url: php
-  },
-]
+  logos: [
+    {
+      name: 'html',
+      url: html,
+    },
+    {
+      name: 'css3',
+      url: css3
+    },
+    {
+      name: 'js',
+      url: js
+    },
+    {
+      name: 'react',
+      url: react
+    },
+    {
+      name: 'angular',
+      url: angular
+    },
+    {
+      name: 'mock',
+      url: mock
+    },
+    {
+      name: '50plus',
+      url: plus
+    },
+  ]
+}
 
 export default WebDevelopmentData;
