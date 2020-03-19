b4610492e33cd15b5bfd861a0e3e3e6a62cd8fae
Austin <austin@goldenowl.asia>
2020-03-19 17:49:55 +0700

    fix UI bug for service sub pages: - content missing - development team logos

--- src/pages/SingleService/index.js
+++ src/pages/SingleService/index.js
@@ -7,11 +7,9 @@ import BreadCrumb from "../../components/BreadCrumb";
 import Footer from "../../components/Footer";
 import "./index.sass";
 import objectToArray from "../../utils/objectToArray";
-import yelllowCopy from '../../assets/images/rectangle-copy.svg';
-
 
 const SingleService = ({ content }) => {
-  const { key } = content;
+
   return (
     <section className="service">
       <div className="container-fluid no-padding">
@@ -20,11 +18,11 @@ const SingleService = ({ content }) => {
         <BreadCrumb>
           <Link to="/services">Services</Link>
           <span>/</span>
-          <p>{key}</p>
+          <p>{content.key}</p>
         </BreadCrumb>
         <section className="service__first-content">
-            <h1>{key}</h1>
-            <p>{content.first.content}</p>
+          <h1>{content.key}</h1>
+          <p>{content.first.content}</p>
           <div className="image-wrapper">
             <img src={content.cover} alt="cover" />
           </div>
@@ -57,41 +55,67 @@ const SingleService = ({ content }) => {
           </div>
         </section>
         <section className="service__third-content">
-          <div className="service__yellow-rectangle d-flex">
-            <div className="col-md-8">
-              <h3>{content.fourth.title}</h3>
-              <p>{content.fourth.content}</p>
-            </div>
-            <div className="col-md-4 service__logos-wrapper d-flex flex-wrap align-items-start">
-              {objectToArray(content.fourth.logos).map(image => (
-                <img
-                  key={image.name}
-                  src={image.url}
-                  alt={image.name}
-                  loading="lazy"
-                />
-              ))}
+          <div id="rectangle" className="service__yellow-rectangle d-flex">
+              <div className="service__yellow-rectangle__text">
+                <h3>{content.fourth.title}</h3>
+                <p>{content.fourth.content}</p>
+              </div>
+              <div className="service__logos-wrapper">
+                <div className="d-flex flex-wrap">
+                  {content.fourth.logos.map(image => (
+                    <img
+                      key={image.name}
+                      src={image.url}
+                      alt={image.name}
+                      loading="lazy"
+                    />
+                  ))}                  
+                </div>
+              </div>
+          </div>
+          <div id="dashed-border" className="service__dotted-square" >
+          <div style={{ visibility: "hidden" }} className="row">
+            {/* append data */}
+              <div className="col-md-8">
+                <h3>{content.fourth.title}</h3>
+                <p>{content.fourth.content}</p>
+              </div>
+              <div className="col-md-4 service__logos-wrapper d-flex flex-wrap align-items-start">
+                {content.fourth.logos.map(image => (
+                  <img
+                    key={image.name}
+                    src={image.url}
+                    alt={image.name}
+                    loading="lazy"
+                  />
+                ))}
+              </div>
             </div>
           </div>
-          {/* <div className="service__dotted-square"/> */}
         </section>
         <section className="service__other-services-container">
-        <h3 className="service__other-services-title">Other services</h3>
-        <div className="d-flex flex-wrap">
-          {objectToArray(content.navs).map(image => (
-            <div key={image.title} className="col-sm-12 col-md-6 service__other-service-wrapper">
-              <Link className="service__other-service" to={`/services/${image.url}`}>
-                <div className="col-sm-3">
-                  <img src={image.imgUrl} alt={image.title} />
-                </div>
-                <div className="col-sm-9">
-                  <h4 className="">{image.title}</h4>
-                </div>
-              </Link>            
-            </div>
-          ))}
-        </div>
-      </section>
+          <h3 className="service__other-services-title">Other services</h3>
+          <div className="d-flex flex-wrap">
+            {objectToArray(content.navs).map(image => (
+              <div
+                key={image.title}
+                className="col-sm-12 col-md-6 service__other-service-wrapper"
+              >
+                <Link
+                  className="service__other-service"
+                  to={`/services/${image.url}`}
+                >
+                  <div className="col-sm-3">
+                    <img src={image.imgUrl} alt={image.title} />
+                  </div>
+                  <div className="col-sm-9">
+                    <h4 className="">{image.title}</h4>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </section>
         <Footer />
       </div>
     </section>
