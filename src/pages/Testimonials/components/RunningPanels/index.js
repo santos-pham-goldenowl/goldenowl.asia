9a774d9084f9d87c5e07464461450dd744eee738
Austin <austin@goldenowl.asia>
2020-03-20 11:28:20 +0700

    solve conversation

--- src/pages/Testimonials/components/RunningPanels/index.js
+++ src/pages/Testimonials/components/RunningPanels/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { firstRow, secondRow } from "../../../../utils/logoSlider";
+
 import "./index.sass";
 
 const RunningPanels = () => (
@@ -8,53 +9,15 @@ const RunningPanels = () => (
     <div className="placeholder-row">
       <div className="row">
         {firstRow.map((item, index) => (
-          <div key={`logo-${index}`} className="col-md-2 d-flex align-items-center justify-content-center">
+          <div
+            key={`logo-${index}`}
+            className="col-md-2 d-flex align-items-center justify-content-center"
+          >
             <img className="placeholder-img" src={item} alt={`logo-${index}`} />
           </div>
         ))}
       </div>
     </div>
-    {/* <div className="running-panels__wrapper">
-      <div className="byKaOp">
-        <div id="first-row" className="kYqVPf">
-          {firstRow.map((item, index) => (
-            <div className="epjWln">
-              <img
-                className="gTxaYB"
-                src={item}
-                alt={`first-row-logo-${index}`}
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="itNaut">
-        <div id="first-row" className="kYqVPf">
-          {firstRow.map((item, index) => (
-            <div className="epjWln">
-              <img
-                className="gTxaYB"
-                src={item}
-                alt={`first-row-logo-${index}`}
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="doEdSu">
-        <div id="first-row" className="kYqVPf">
-          {firstRow.map((item, index) => (
-            <div className="epjWln">
-              <img
-                className="gTxaYB"
-                src={item}
-                alt={`first-row-logo-${index}`}
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-    </div> */}
   </section>
 );
 
