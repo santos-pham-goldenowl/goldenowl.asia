a733c1866d2d777819b41842e66bb5a191983ce3
Austin <austin@goldenowl.asia>
2020-03-20 08:21:44 +0700

    - fix wrong technology typos and styles

--- src/pages/Technology/components/Guarantee/index.js
+++ src/pages/Technology/components/Guarantee/index.js
@@ -10,37 +10,37 @@ const Guarantee = () => (
       <div className="col-md-4">
         <h5>Deliver the most high-quality product</h5>
         <br />
-        <div className="d-flex">
+        <div className="d-flex guarantee__line">
           <CheckPoint />
           <p>Elegant design</p>
         </div>
         <br />
-        <div className="d-flex">
+        <div className="d-flex guarantee__line">
           <CheckPoint />
           <p>Absolute security</p>
         </div>
         <br />
-        <div className="d-flex">
+        <div className="d-flex guarantee__line">
           <CheckPoint />
           <p>High degree of stability</p>
         </div>
       </div>
       <div className="col-md-4">
-        <h5>
+        <h5 id="middle-title">
           Provide the best solution so that customer gets the greatest benefit
         </h5>
         <br />
-        <div className="d-flex">
+        <div className="d-flex guarantee__line">
           <CheckPoint />
           <p>Meet the customer requirements with latest technologies</p>
         </div>
         <br />
-        <div className="d-flex">
+        <div className="d-flex guarantee__line">
           <CheckPoint />
           <p>Provide a scalable system</p>
         </div>
         <br />
-        <div className="d-flex">
+        <div className="d-flex guarantee__line">
           <CheckPoint />
           <p>With lowest cost solution</p>
         </div>
@@ -48,14 +48,14 @@ const Guarantee = () => (
       <div className="col-md-4">
         <h5>Provide maintenance/ improvement</h5>
         <br />
-        <div className="d-flex">
+        <div className="d-flex guarantee__line">
           <CheckPoint />
           <p>
             Endurance: Endurance before errors, large number of users/request
           </p>
         </div>
         <br />
-        <div className="d-flex">
+        <div className="d-flex guarantee__line">
           <CheckPoint />
           <p>Easy to extends in the future</p>
         </div>
