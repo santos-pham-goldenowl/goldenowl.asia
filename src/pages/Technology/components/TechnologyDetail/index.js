b191c0b7d4aafdb64382552be96427456e90d18c
Austin <austin@goldenowl.asia>
2020-03-31 08:24:31 +0700

    apply mixins and mobile responsive for Technology Page

--- src/pages/Technology/components/TechnologyDetail/index.js
+++ src/pages/Technology/components/TechnologyDetail/index.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React from "react";
 
-import TabContainer from '../../../../components/Tabs/TechTabs'
+import TabContainer from "../../../../components/Tabs/TechTabs";
+import useMobileWidth from "../../../../utils/hooks/useMobileWidth";
 
-import './index.sass'
+import "./index.sass";
 
-const TechnologyDetail = () => (
-  <section className="technology-detail">
-    <div className="row">
-      <h3 className="d-block text-center w-100">Tech we use</h3>
-      <p className="technology-detail__description d-block text-center w-100">
-        We apply the most modern technology to every single project to make it
-        <br />
-        become the state of the art.
-      </p>
-      <div className="col-12">
-        <TabContainer />
+const TechnologyDetail = () => {
+  const isMobile = useMobileWidth();
+  return (
+    <section className="technology-detail">
+      <div className="row">
+        <h3 className="d-block text-center w-100">Tech we use</h3>
+        <p className="technology-detail__description d-block text-center w-100">
+          We apply the most modern technology to every single project to make it
+          {isMobile || <br />}
+          {' '}become{' '} 
+          {isMobile && <br />}
+          the state of the art.
+        </p>
+        <div className="col-12">
+          <TabContainer />
+        </div>
       </div>
-    </div>
-  </section>
-)
+    </section>
+  );
+};
 
-export default TechnologyDetail
+export default TechnologyDetail;
