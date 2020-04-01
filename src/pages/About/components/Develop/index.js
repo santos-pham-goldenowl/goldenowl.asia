6c6bad2f04b7deb9a669e7b0737d7a21d6e078e0
Austin <austin@goldenowl.asia>
2020-04-01 10:32:49 +0700

    replace vscode image on mobile

--- src/pages/About/components/Develop/index.js
+++ src/pages/About/components/Develop/index.js
@@ -1,30 +1,47 @@
-import React from 'react'
+import React from "react";
 
-import code from '../../../../assets/images/code.png'
-import './index.sass'
+import code from "../../../../assets/images/code.png";
+import mobileCode from "../../../../assets/images/mobileCode.png";
+import "./index.sass";
+import useMobileWidth from "../../../../utils/hooks/useMobileWidth";
 
-const Develop = () => (
-  <section className="develop">
-    <h2 className="develop__title">
-      Develop with tools you
-      <br />
-      already love
-    </h2>
-    <div className="row develop__content">
-      <div className="col-12 col-md-4 develop__content-text">
-        <h3>
-          Work with your beloved and familiar language
-        </h3>
-        <p>
-          Golden Owl - we have a team that is expert at different programming languages so whatever your favourite language is, we are always here to turn it into your wanted product.
-        </p>
-      </div>
-      <div className="col-12 col-md-7 ml-auto image-wrapper">
-        <div className="rectangle" />
-        <img className="develop__image" src={code} alt="code" />
-      </div>
+const Develop = () => {
+  const isMobile = useMobileWidth();
+
+  const mobileImageRender = () => (
+    <div className="col-12 image-wrapper">
+      <img className="develop__image-placeholder invisible" src={mobileCode} alt="code" />
+      <img className="develop__image" src={mobileCode} alt="code" />
     </div>
-  </section>
-)
+  )
 
-export default Develop
+  const defaultImageRender = () => (
+    <div className="col-md-7 ml-auto image-wrapper">
+      <div className="rectangle" />
+      <img className="develop__image" src={code} alt="code" />
+    </div>
+  )
+
+  return (
+    <section className="develop">
+      <h2 className="develop__title">
+        Develop with tools you
+        <br />
+        already love
+      </h2>
+      <div className="row develop__content">
+        <div className="col-12 col-md-4 develop__content-text">
+          <h3>Work with your beloved and familiar language</h3>
+          <p>
+            Golden Owl - we have a team that is expert at different programming
+            languages so whatever your favourite language is, we are always here
+            to turn it into your wanted product.
+          </p>
+        </div>
+        {isMobile ? mobileImageRender() : defaultImageRender() }
+      </div>
+    </section>
+  );
+};
+
+export default Develop;
