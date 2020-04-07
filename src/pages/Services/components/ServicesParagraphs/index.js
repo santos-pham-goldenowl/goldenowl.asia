1f4d78808b182e2e553340fd33b85ec3c033bb73
Austin <austin@goldenowl.asia>
2020-04-07 16:10:29 +0700

    services-page

--- src/pages/Services/components/ServicesParagraphs/index.js
+++ src/pages/Services/components/ServicesParagraphs/index.js
@@ -8,8 +8,8 @@ import './index.sass'
 const ServicesParagraphs = () => (
   <section className="services-paragraphs">
     <div className="row">
-      {objectToArray(ServicesDetailParagraphs).map((paragraph) => (
-        <div key={paragraph.key} className="col-12 col-md-6">
+      {objectToArray(ServicesDetailParagraphs).map((paragraph, index) => (
+        <div id={`services-paragraph-${index}`} key={paragraph.key} className="col-12 col-md-6">
           <div className="card">
             <h4>{paragraph.key}</h4>
             <p>{paragraph.content}</p>
