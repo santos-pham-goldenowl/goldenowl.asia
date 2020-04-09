a2f2166ca98bdf591e32d1447d9eac11e6b8cdc7
Austin <austin@goldenowl.asia>
2020-04-09 16:49:15 +0700

    add fixed-top component

--- src/components/FixedTopHeader/index.js
+++ src/components/FixedTopHeader/index.js
@@ -2,32 +2,34 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 import ServicesData from 'utils/servicesMenuContent'
-import arrowRight from 'assets/images/arrow-right.svg'
-import arrowDown from 'assets/images/chevron-down-solid.svg'
+import arrowRight from 'assets/images/arrow-right-white.png'
+import arrowDown from 'assets/images/chevron-down-solid-white.png'
+import goWhite from 'assets/images/goWhite.png'
 
 import './index.sass'
 import './service.sass'
 
-class SubHeader extends Component {
+class FixedTopHeader extends Component {
   render() {
     return (
-      <header id="sub-header" className="sub-header">
-        <div className="d-flex align-items-center sub-header__button">
+      <header id="fixed-top-header" className="fixed-top-header d-none fixed-top">
+        <img src={goWhite} className="fixed-top-header__logo mr-auto" />
+        <div className="d-flex align-items-center fixed-top-header__button">
           <Link to="/">
-            <h4 className="sub-header__text">Home</h4>
+            <h4 className="fixed-top-header__text">Home</h4>
           </Link>
         </div>
         {/* Services Dropdown-start */}
         <div className="dropdown">
           <div
-            className="align-items-center sub-header__button"
+            className="align-items-center fixed-top-header__button"
             id="services-dropdown"
           >
             <Link
               className="d-flex align-items-center no-hover-text-decoration"
               to="/services"
             >
-              <h4 className="sub-header__text">Services</h4>
+              <h4 className="fixed-top-header__text">Services</h4>
               <img className="arrow-down" src={arrowDown} alt="arrow-down" />
             </Link>
           </div>
@@ -52,23 +54,23 @@ class SubHeader extends Component {
           </div>
         </div>
         {/* Services Dropdown-end */}
-        <div className="d-flex align-items-center sub-header__button">
+        <div className="d-flex align-items-center fixed-top-header__button">
           <Link to="/technologies">
-            <h4 className="sub-header__text">Technology</h4>
+            <h4 className="fixed-top-header__text">Technology</h4>
           </Link>
         </div>
-        <div className="d-flex align-items-center sub-header__button">
+        <div className="d-flex align-items-center fixed-top-header__button">
           <Link to="/testimonial">
-            <h4 className="sub-header__text">Testimonial</h4>
+            <h4 className="fixed-top-header__text">Testimonial</h4>
           </Link>
         </div>
-        <div className="sub-header__contact-wrapper d-flex align-items-center justify-content-center">
+        <div className="fixed-top-header__contact-wrapper d-flex align-items-center justify-content-center">
           <Link to="/contact-us" className="d-flex align-items-center">
-            <h4 className="sub-header__contact-title sub-header__text">
+            <h4 className="fixed-top-header__contact-title fixed-top-header__text">
               Contact us
             </h4>
             <img
-              className="sub-header__arrow-right"
+              className="fixed-top-header__arrow-right"
               src={arrowRight}
               alt="GO-contact-us"
             />
@@ -79,4 +81,4 @@ class SubHeader extends Component {
   }
 }
 
-export default SubHeader
+export default FixedTopHeader
