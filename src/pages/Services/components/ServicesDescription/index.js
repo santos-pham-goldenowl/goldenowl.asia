b08dc978429bdff5486f0f2993a2d51f2c065ce8
Austin <austin@goldenowl.asia>
2020-04-09 17:57:34 +0700

    correct styles, content

--- src/pages/Services/components/ServicesDescription/index.js
+++ src/pages/Services/components/ServicesDescription/index.js
@@ -26,7 +26,7 @@ const ServicesDescription = () => {
             We provide software solutions, mostly about website and mobile applications for a wide variety of fields, from finance to commerce and so on. We are proud of being very selective and have one of the best full-stacked developer training programs in Ho Chi Minh City.
             <br />
             <br />
-            Lorem ipsum dolor sit amet, te alii munere duo, vel nostrud iuvaret te. In nonumy salutatus vel. Verear ponderum te sed, vis in putent explicari. Nam tempor inimicus patrioque ei, an usu epicuri detracto gubergren. Atqui minim corrumpit cu usu, cu denique comprehensam per. Adhuc inimicus constituto ut ius, eros civibus ei quo, vel eu labores fuisset percipitur.
+            Lots of clients come to us to receive best service and they have been besides us for a very long time. From the beginning when building a platform to maintenance process, we do them all with perfect result.
           </p>
         </div>
         <div className="col-md-6 ml-auto">
