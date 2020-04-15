eafb68639b7b51246cf9a08e27255cebe8da6b46
Austin <austin@goldenowl.asia>
2020-04-15 09:17:53 +0700

    apply sendgrid to contact page

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 // Express requirements
-import bodyParser from 'body-parser';
-import compression from 'compression';
-import express from 'express';
-import morgan from 'morgan';
-import path from 'path';
-import Loadable from 'react-loadable';
-import cookieParser from 'cookie-parser';
+import bodyParser from "body-parser";
+import compression from "compression";
+import express from "express";
+import morgan from "morgan";
+import path from "path";
+import Loadable from "react-loadable";
+import cookieParser from "cookie-parser";
+const cors = require("cors");
+require('dotenv').config();
 
 // Our loader - this basically acts as the entry point for each page load
-import loader from './loader';
+import loader from "./loader";
 
 // Create our express app using the port optionally specified
 const app = express();
@@ -18,12 +20,66 @@ const PORT = process.env.PORT || 3000;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cookieParser());
+app.use(cors());
+
+// Send email
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // Set up homepage, static assets, and capture everything else
-app.use(express.Router().get('/', loader));
-app.use(express.static(path.resolve(__dirname, '../build')));
+app.use(express.Router().get("/", loader));
+app.use(express.static(path.resolve(__dirname, "../build")));
+
+app.use(express.Router().post("/send-email", (req, res) => {
+  const { firstName, lastName, email, phoneNum, company, country, subject, message: goMessage } = req.body;
+
+  const msg = {
+    to: "stephanyhcm.go@gmail.com",
+    from: "austin@goldenowl.asia",
+    subject: `From [GO Website]: ${subject}`,
+    html: `<head>
+            <style>
+                body {font-family: "Roboto";}
+                h3 {color: #0096ff;}
+                p {font-size: 15px;}
+                em {color: #4cb5ff;}
+                u {color: #4cb5ff;}
+                b {color: #4cb5ff;}
+                a {color:#4cb5ff;}
+                span {color: #4cb5ff; font-weight: bold;}
+                .message-section em {font-style: italic;}
+                .client-info { font-style: italic; font-weight: bold; }
+                .client-info span {color: #4cb5ff; font-weight: normal;}
+            </style>
+          </head>          
+          <body>
+            <h3>Hi Admin,</h3>
+            <p class="opening">A visitor whose name is&nbsp;${`${firstName} ${lastName}`}&nbsp;sent you a message with the
+                following content:</p>
+                <p class="message-section">&nbsp;&nbsp;<em>${goMessage}</em></p>
+                <p class="client-info">${company},&nbsp;${country}</p>
+                <p class="client-info"><u>Visitor's email:</u>&nbsp;${email}</p>
+                <p class="client-info"><u>Visitor's phone number:</u>&nbsp;${phoneNum}</p>
+          </body>`,
+  };
+
+  sgMail.send(msg).then(
+    () => {
+      res.json({
+        message: "Message sent!"
+      })
+    },
+    (error) => {
+      res.json({
+        error: true,
+        message: `Error: ${error}`,
+      });
+    }
+  );
+}));
+
 app.use(loader);
 
 // We tell React Loadable to load all required assets and start listening - ROCK AND ROLL!
@@ -32,19 +88,19 @@ Loadable.preloadAll().then(() => {
 });
 
 // Handle the bugs somehow
-app.on('error', (error) => {
-  if (error.syscall !== 'listen') {
+app.on("error", (error) => {
+  if (error.syscall !== "listen") {
     throw error;
   }
 
-  const bind = typeof PORT === 'string' ? `Pipe ${PORT}` : `Port ${PORT}`;
+  const bind = typeof PORT === "string" ? `Pipe ${PORT}` : `Port ${PORT}`;
 
   switch (error.code) {
-    case 'EACCES':
+    case "EACCES":
       console.error(`${bind} requires elevated privileges`);
       process.exit(1);
       break;
-    case 'EADDRINUSE':
+    case "EADDRINUSE":
       console.error(`${bind} is already in use`);
       process.exit(1);
       break;
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,26 +1,42 @@
-import React, { createRef } from 'react';
-import { Link } from 'react-router-dom';
-import Helmet from 'react-helmet';
+import React, { createRef, useState } from "react";
+import { Link } from "react-router-dom";
+import Helmet from "react-helmet";
+import axios from "axios";
 
-import Footer from '../../components/Footer';
-import SubHeader from '../../components/SubHeader';
-import MainHeader from '../../components/MainHeader';
-import BreadCrumb from '../../components/BreadCrumb';
-import CheckPoint from '../../components/CheckPoint';
-import ArrowRight from '../../components/ArrowRight';
-import FixedTopHeader from '../../components/FixedTopHeader';
-import FixedTopBreadCrumb from '../../components/FixedTopBreadCrumb';
+import Footer from "../../components/Footer";
+import SubHeader from "../../components/SubHeader";
+import MainHeader from "../../components/MainHeader";
+import BreadCrumb from "../../components/BreadCrumb";
+import CheckPoint from "../../components/CheckPoint";
+import ArrowRight from "../../components/ArrowRight";
+import FixedTopHeader from "../../components/FixedTopHeader";
+import FixedTopBreadCrumb from "../../components/FixedTopBreadCrumb";
 
-import stickyTrigger from '../../utils/stickyTrigger';
-import useMobileWidth from '../../utils/hooks/useMobileWidth';
-import useScrollDirection from '../../utils/hooks/useScrollDirection';
+import stickyTrigger from "../../utils/stickyTrigger";
+import countries from "../../utils/countries";
+import useMobileWidth from "../../utils/hooks/useMobileWidth";
+import useScrollDirection from "../../utils/hooks/useScrollDirection";
 
-import './index.sass';
+import "./index.sass";
+import objectToArray from "../../utils/objectToArray";
 
 const ServicesSubPage = () => {
   const isMobile = useMobileWidth();
   const pageContent = createRef();
   const scrollDirection = useScrollDirection();
+  const [info, setInfo] = useState();
+
+  const onInputChange = (label, data) => setInfo({ ...info, [label]: data });
+
+  const sendEmail = () =>
+    axios
+      .post("/send-email", info)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 
   window.onscroll = () => stickyTrigger(scrollDirection);
 
@@ -45,30 +61,34 @@ const ServicesSubPage = () => {
               <div className="col-md-5">
                 {isMobile ? (
                   <h1>
-                    Chat with our
-                    {' '}
-                    <br />
-                    {' '}
-                    sales team
+                    Chat with our <br /> sales team
                   </h1>
-                ) : <h1>Chat with our sales team</h1>}
+                ) : (
+                  <h1>Chat with our sales team</h1>
+                )}
                 <p className="contact-form__introduction">
                   Whether you’re a start-up, we’d love to chat about our
                   products and make a personalised plan that fits your business.
                 </p>
                 <div className="d-flex">
                   <CheckPoint />
-                  <p className="contact-form__reason">Learn more about Golden Owl</p>
+                  <p className="contact-form__reason">
+                    Learn more about Golden Owl
+                  </p>
                 </div>
                 <br />
                 <div className="d-flex">
                   <CheckPoint />
-                  <p className="contact-form__reason">Find the right solution for your needs</p>
+                  <p className="contact-form__reason">
+                    Find the right solution for your needs
+                  </p>
                 </div>
                 <br />
                 <div className="d-flex">
                   <CheckPoint />
-                  <p className="contact-form__reason">Learn more about Golden Owl</p>
+                  <p className="contact-form__reason">
+                    Learn more about Golden Owl
+                  </p>
                 </div>
               </div>
               <div className="col-md-6 ml-auto">
@@ -79,6 +99,9 @@ const ServicesSubPage = () => {
                       <input
                         className="form-control"
                         placeholder="Your first name"
+                        onChange={(e) =>
+                          onInputChange("firstName", e.target.value)
+                        }
                       />
                     </div>
                     <div className="col-md-6">
@@ -86,6 +109,9 @@ const ServicesSubPage = () => {
                       <input
                         className="form-control"
                         placeholder="Your last name"
+                        onChange={(e) =>
+                          onInputChange("lastName", e.target.value)
+                        }
                       />
                     </div>
                     <div className="col-md-6">
@@ -94,6 +120,7 @@ const ServicesSubPage = () => {
                         required
                         className="form-control"
                         placeholder="Your email"
+                        onChange={(e) => onInputChange("email", e.target.value)}
                       />
                     </div>
                     <div className="col-md-6">
@@ -101,6 +128,9 @@ const ServicesSubPage = () => {
                       <input
                         className="form-control"
                         placeholder="+84 212 2223 333"
+                        onChange={(e) =>
+                          onInputChange("phoneNum", e.target.value)
+                        }
                       />
                     </div>
                     <div className="col-md-6">
@@ -108,11 +138,23 @@ const ServicesSubPage = () => {
                       <input
                         className="form-control"
                         placeholder="Your company"
+                        onChange={(e) =>
+                          onInputChange("company", e.target.value)
+                        }
                       />
                     </div>
                     <div className="col-md-6">
                       <label>Country</label>
-                      <select className="form-control" />
+                      <select
+                        className="form-control"
+                        onChange={(e) =>
+                          onInputChange("country", e.target.value)
+                        }
+                      >
+                        {countries.map((item) => (
+                          <option value={item}>{item}</option>
+                        ))}
+                      </select>
                     </div>
                     <div className="col-md-12">
                       <label>Subject*</label>
@@ -120,6 +162,9 @@ const ServicesSubPage = () => {
                         required
                         className="form-control"
                         placeholder="Overview in the few words"
+                        onChange={(e) =>
+                          onInputChange("subject", e.target.value)
+                        }
                       />
                     </div>
                     <div className="col-md-12">
@@ -129,10 +174,16 @@ const ServicesSubPage = () => {
                         rows={isMobile ? 7 : 10}
                         className="form-control"
                         placeholder="How can we help you?"
+                        onChange={(e) =>
+                          onInputChange("message", e.target.value)
+                        }
                       />
                     </div>
                     <div className="col-md-12 pr-0">
-                      <div className="contact-form__send-wrapper d-flex">
+                      <div
+                        onClick={sendEmail}
+                        className="contact-form__send-wrapper d-flex"
+                      >
                         <div className="send-rectangle">
                           <div className="row h-100">
                             <div className="col-6 mt-auto">
