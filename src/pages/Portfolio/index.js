24227a3126a1080678b8381858389e42f19f6158
Austin <austin@goldenowl.asia>
2020-04-17 18:08:54 +0700

    update og image

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -8,6 +8,7 @@ import PortfolioInteresting from '../../components/PortfolioInteresting';
 
 import allTab, { mobileTab, webTab } from '../../utils/portfolioTabContent';
 
+import companyLogo from '../../assets/images/GoldenOwlLogo.png';
 import './index.sass';
 
 const Portfolio = () => {
@@ -36,7 +37,7 @@ const Portfolio = () => {
         <meta content="N_qR6-efA-BOE-NPwuBG69fmJ-UG_wDHG34i4ixSlug" name="google-site-verification" />
         <meta content="Golden Owl - We do Ruby on Rails, NodeJS, ReactJS and React Native. We follow Agile &amp; TDD practice and cool softwares like Github, Basecamp, Slack in our daily work to provide best communication and transparency to clients. Our services include web development, mobile development, head hunting and more." name="description" /><meta content="Golden Owl - Ruby on Rails, NodeJS, ReactJS and React Native" property="og:title" />
         <meta content="Golden Owl - We do Ruby on Rails, NodeJS, ReactJS and React Native. We follow Agile &amp; TDD practice and cool softwares like Github, Basecamp, Slack in our daily work to provide best communication and transparency to clients. Our services include web development, mobile development, head hunting and more." property="og:description" />
-        <meta content="http://www.goldenowl.asia/assets/background-home.jpg" property="og:image" />
+        <meta content={companyLogo} property="og:image" />
         <meta name="csrf-param" content="authenticity_token" />
         <meta name="csrf-token" content="TdCfVtfoL4PbYbE7oJMWiiM/8pGrMTiGoHOSDR5SnWS76hsk9b6nMmeMSr8my4ILM288ym8oPwbE1dLlwuogbg==" />
       </Helmet>
--- src/pages/ServicesSubPage/index.js
+++ src/pages/ServicesSubPage/index.js
@@ -18,6 +18,7 @@ import useScrollDirection from '../../utils/hooks/useScrollDirection';
 import coverPlaceholder from '../../assets/images/service/cover-min.png';
 import mCoverPlaceholder from '../../assets/images/service/mCover-min.png';
 
+import companyLogo from '../../assets/images/GoldenOwlLogo.png';
 import './index.sass';
 
 const ServicesSubPage = ({ content }) => {
@@ -38,7 +39,7 @@ const ServicesSubPage = ({ content }) => {
         <meta content="N_qR6-efA-BOE-NPwuBG69fmJ-UG_wDHG34i4ixSlug" name="google-site-verification" />
         <meta content="Golden Owl - We do Ruby on Rails, NodeJS, ReactJS and React Native. We follow Agile &amp; TDD practice and cool softwares like Github, Basecamp, Slack in our daily work to provide best communication and transparency to clients. Our services include web development, mobile development, head hunting and more." name="description" /><meta content="Golden Owl - Ruby on Rails, NodeJS, ReactJS and React Native" property="og:title" />
         <meta content="Golden Owl - We do Ruby on Rails, NodeJS, ReactJS and React Native. We follow Agile &amp; TDD practice and cool softwares like Github, Basecamp, Slack in our daily work to provide best communication and transparency to clients. Our services include web development, mobile development, head hunting and more." property="og:description" />
-        <meta content="http://www.goldenowl.asia/assets/background-home.jpg" property="og:image" />
+        <meta content={companyLogo} property="og:image" />
         <meta name="csrf-param" content="authenticity_token" />
         <meta name="csrf-token" content="TdCfVtfoL4PbYbE7oJMWiiM/8pGrMTiGoHOSDR5SnWS76hsk9b6nMmeMSr8my4ILM288ym8oPwbE1dLlwuogbg==" />
       </Helmet>
