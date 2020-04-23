b0570779bdc76c8b696579aeaa15f1a53fbb4726
Austin <austin@goldenowl.asia>
2020-04-23 10:09:25 +0700

    init sitemap builder

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,23 +1,7 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
 
 import ScrollToTop from '../../components/ScrollToTop';
-
-import Home from '../Home';
-import About from '../About';
-import Contact from '../Contact';
-import Services from '../Services';
-import ServicesSubPage from '../ServicesSubPage';
-import Technology from '../Technology';
-import PortfolioSubPage from '../PortfolioSubPage';
-import Testimonials from '../Testimonials';
-import Portfolio from '../Portfolio';
-import Blog from '../Blog';
-import NotFound from '../NotFound';
-
-import objectToArray from '../../utils/objectToArray';
-import portfolioDetails from '../../utils/portfolioDetails';
-import singleServiceDetails from '../../utils/singleServiceDetails';
+import AppRoutes from '../../routes'
 
 import './index.sass';
 
@@ -30,37 +14,7 @@ const App = () => (
   <>
     <ScrollToTop />
     <section id="app" className="App">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about-us" component={About} />
-        <Route exact path="/technologies" component={Technology} />
-        <Route exact path="/testimonial" component={Testimonials} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        {
-          objectToArray(portfolioDetails).map((portfolio) => (
-            <Route
-              exact
-              key={portfolio.key}
-              path={`/portfolio/${portfolio.url}`}
-              component={() => <PortfolioSubPage content={portfolio} />}
-            />
-          ))
-        }
-        <Route path="/blog" component={Blog} />
-        <Route path="/contact-us" component={Contact} />
-        <Route exact path="/services" component={Services} />
-        {
-          objectToArray(singleServiceDetails).map((service) => (
-            <Route
-              exact
-              key={service.key}
-              path={`/services/${service.url}`}
-              component={() => <ServicesSubPage content={service} />}
-            />
-          ))
-        }
-        <Route path="*" component={NotFound} />
-      </Switch>
+      <AppRoutes />
     </section>
   </>
 );
---
+++ src/routes/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Route, Switch } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Contact from '../pages/Contact';
+import Services from '../pages/Services';
+import ServicesSubPage from '../pages/ServicesSubPage';
+import Technology from '../pages/Technology';
+import PortfolioSubPage from '../pages/PortfolioSubPage';
+import Testimonials from '../pages/Testimonials';
+import Portfolio from '../pages/Portfolio';
+import Blog from '../pages/Blog';
+import NotFound from '../pages/NotFound';
+
+import objectToArray from '../utils/objectToArray';
+import portfolioDetails from '../utils/portfolioDetails';
+import singleServiceDetails from '../utils/singleServiceDetails';
+
+const index = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about-us" component={About} />
+      <Route exact path="/technologies" component={Technology} />
+      <Route exact path="/testimonial" component={Testimonials} />
+      <Route exact path="/portfolio" component={Portfolio} />
+      {objectToArray(portfolioDetails).map((portfolio) => (
+        <Route
+          exact
+          key={portfolio.key}
+          path={`/portfolio/${portfolio.url}`}
+          component={() => <PortfolioSubPage content={portfolio} />}
+        />
+      ))}
+      <Route path="/blog" component={Blog} />
+      <Route path="/contact-us" component={Contact} />
+      <Route exact path="/services" component={Services} />
+      {objectToArray(singleServiceDetails).map((service) => (
+        <Route
+          exact
+          key={service.key}
+          path={`/services/${service.url}`}
+          component={() => <ServicesSubPage content={service} />}
+        />
+      ))}
+      <Route path="*" component={NotFound} />
+    </Switch>
+  );
+};
+
+export default index;
