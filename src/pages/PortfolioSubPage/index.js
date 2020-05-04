29e303e564f1ac9eb84e70d5c386021009c53c6d
Austin <austin@goldenowl.asia>
2020-05-04 11:43:25 +0700

    update next project link

--- src/pages/PortfolioSubPage/index.js
+++ src/pages/PortfolioSubPage/index.js
@@ -1,77 +1,133 @@
-import React from 'react';
-import Helmet from 'react-helmet';
+import React from "react";
+import Helmet from "react-helmet";
+import { Link } from "react-router-dom";
 
-import PortfolioHeader from '../../components/PortfolioHeader';
-import PortfolioInteresting from '../../components/PortfolioInteresting';
+import PortfolioHeader from "../../components/PortfolioHeader";
+import PortfolioInteresting from "../../components/PortfolioInteresting";
 
-import companyLogo from '../../assets/images/GoldenOwlLogo.png';
-import './index.sass';
+import randomProjects from "../../utils/portfolioTabContent";
+import useLocalStorage from "../../utils/hooks/useLocalStorage";
 
-const Services = ({ content }) => (
-  <section>
-    <Helmet>
-      <title>{`${content.key} - Golden Owl`}</title>
-      <link href="https://www.goldenowl.asia/home/amp" rel="amphtml" />
-      <link href="https://www.goldenowl.asia/home/home" rel="canonical" />
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta content="N_qR6-efA-BOE-NPwuBG69fmJ-UG_wDHG34i4ixSlug" name="google-site-verification" />
-      <meta content="Golden Owl - We do Ruby on Rails, NodeJS, ReactJS and React Native. We follow Agile &amp; TDD practice and cool softwares like Github, Basecamp, Slack in our daily work to provide best communication and transparency to clients. Our services include web development, mobile development, head hunting and more." name="description" /><meta content="Golden Owl - Ruby on Rails, NodeJS, ReactJS and React Native" property="og:title" />
-      <meta content="Golden Owl - We do Ruby on Rails, NodeJS, ReactJS and React Native. We follow Agile &amp; TDD practice and cool softwares like Github, Basecamp, Slack in our daily work to provide best communication and transparency to clients. Our services include web development, mobile development, head hunting and more." property="og:description" />
-      <meta content={companyLogo} property="og:image" />
-      <meta name="csrf-param" content="authenticity_token" />
-      <meta name="csrf-token" content="TdCfVtfoL4PbYbE7oJMWiiM/8pGrMTiGoHOSDR5SnWS76hsk9b6nMmeMSr8my4ILM288ym8oPwbE1dLlwuogbg==" />
-    </Helmet>
-    <div className="container-fluid no-padding">
-      <PortfolioHeader />
-      <section className="portfolio-details">
-        <h1>{content.key}</h1>
-        <div className="row">
-          <div className="col-12 portfolio-details__cover">
-            <img src={content.cover} alt={`${content.key} cover`} loading="lazy" />
-          </div>
-          <div className="col-12 portfolio-details__first-paragraph d-flex align-items-start">
-            <h3 className="portfolio-details__item-title">Overview</h3>
-            <p className="portfolio-details__item-content">
-              {content.overview}
-            </p>
-          </div>
-          <div className="col-12 portfolio-details__second-paragraph d-flex align-items-start">
-            <h3 className="portfolio-details__item-title">
-              Technologies
-            </h3>
-            <ul className="portfolio-details__item-content no-margin-bottom">
-              {content.techs.map((item) => (
-                <li key={item} className="d-flex">
-                  <p>{item}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
-          {content.img.map((image, index) => (
-            <div
-              className={`${image.col > 4 ? 'col-12' : 'col-6'} col-md-${image.col} portfolio-details__image`}
-              key={`${content.key}-${index}`}
-            >
-              <img src={image.src} alt={`${content.key} img no.${index}`} loading="lazy" />
+import companyLogo from "../../assets/images/GoldenOwlLogo.png";
+import "./index.sass";
+
+const PortfolioSubPage = ({ content }) => {
+  const [prevProject, setPrevProject] = useLocalStorage("prevProject", "none");
+
+  // exclude current & previous
+  for (let i = randomProjects.length - 1; i >= 0; i--) {
+    if (
+      randomProjects[i].content.includes(content.key) ||
+      randomProjects[i].content.includes(prevProject)
+    ) {
+      randomProjects.splice(i, 1);
+    }
+  }
+
+  const randomProject =
+    randomProjects[Math.floor(Math.random() * randomProjects.length)];
+  console.log("PortfolioSubPage -> randomProjects", randomProjects);
+
+  const randomProjectText = randomProject.content.split(" – ");
+  const randomProjectUrl = randomProject.url;
+
+  return (
+    <section>
+      <Helmet>
+        <title>{`${content.key} - Golden Owl`}</title>
+        <link href="https://www.goldenowl.asia/home/amp" rel="amphtml" />
+        <link href="https://www.goldenowl.asia/home/home" rel="canonical" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta
+          content="N_qR6-efA-BOE-NPwuBG69fmJ-UG_wDHG34i4ixSlug"
+          name="google-site-verification"
+        />
+        <meta
+          content="Golden Owl - We do Ruby on Rails, NodeJS, ReactJS and React Native. We follow Agile &amp; TDD practice and cool softwares like Github, Basecamp, Slack in our daily work to provide best communication and transparency to clients. Our PortfolioSubPages include web development, mobile development, head hunting and more."
+          name="description"
+        />
+        <meta
+          content="Golden Owl - Ruby on Rails, NodeJS, ReactJS and React Native"
+          property="og:title"
+        />
+        <meta
+          content="Golden Owl - We do Ruby on Rails, NodeJS, ReactJS and React Native. We follow Agile &amp; TDD practice and cool softwares like Github, Basecamp, Slack in our daily work to provide best communication and transparency to clients. Our services include web development, mobile development, head hunting and more."
+          property="og:description"
+        />
+        <meta content={companyLogo} property="og:image" />
+        <meta name="csrf-param" content="authenticity_token" />
+        <meta
+          name="csrf-token"
+          content="TdCfVtfoL4PbYbE7oJMWiiM/8pGrMTiGoHOSDR5SnWS76hsk9b6nMmeMSr8my4ILM288ym8oPwbE1dLlwuogbg=="
+        />
+      </Helmet>
+      <div className="container-fluid no-padding">
+        <PortfolioHeader />
+        <section className="portfolio-details">
+          <h1>{content.key}</h1>
+          <div className="row">
+            <div className="col-12 portfolio-details__cover">
+              <img
+                src={content.cover}
+                alt={`${content.key} cover`}
+                loading="lazy"
+              />
             </div>
-          ))}
-          <div className="col-md-12 portfolio-details__third-paragraph d-flex align-items-start">
-            {content.brief.title}
-            <p className="portfolio-details__item-content">
-              {content.brief.description}
-            </p>
-          </div>
-          <div className="col-12">
-            <div className="portfolio-details__next-project">
-              <h3>Up next: Personifi</h3>
-              <p>Where to find the most suitable personal loan</p>
+            <div className="col-12 portfolio-details__first-paragraph d-flex align-items-start">
+              <h3 className="portfolio-details__item-title">Overview</h3>
+              <p className="portfolio-details__item-content">
+                {content.overview}
+              </p>
+            </div>
+            <div className="col-12 portfolio-details__second-paragraph d-flex align-items-start">
+              <h3 className="portfolio-details__item-title">Technologies</h3>
+              <ul className="portfolio-details__item-content no-margin-bottom">
+                {content.techs.map((item) => (
+                  <li key={item} className="d-flex">
+                    <p>{item}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {content.img.map((image, index) => (
+              <div
+                className={`${image.col > 4 ? "col-12" : "col-6"} col-md-${
+                  image.col
+                } portfolio-details__image`}
+                key={`${content.key}-${index}`}
+              >
+                <img
+                  src={image.src}
+                  alt={`${content.key} img no.${index}`}
+                  loading="lazy"
+                />
+              </div>
+            ))}
+            <div className="col-md-12 portfolio-details__third-paragraph d-flex align-items-start">
+              {content.brief.title}
+              <p className="portfolio-details__item-content">
+                {content.brief.description}
+              </p>
+            </div>
+            <div className="col-12">
+              <Link
+                onClick={() => {
+                  setPrevProject(content.key);
+                }}
+                to={`/portfolio/${randomProjectUrl}`}
+              >
+                <div className="portfolio-details__next-project">
+                  <h3>{`Up next: ${randomProjectText[0]}`}</h3>
+                  <p>{randomProjectText[1]}</p>
+                </div>
+              </Link>
             </div>
           </div>
-        </div>
-      </section>
-      <PortfolioInteresting />
-    </div>
-  </section>
-);
+        </section>
+        <PortfolioInteresting />
+      </div>
+    </section>
+  );
+};
 
-export default Services;
+export default PortfolioSubPage;
