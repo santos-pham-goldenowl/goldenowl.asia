0faa54289bb8893960d967fafe556b105256f84c
Austin <austin@goldenowl.asia>
2020-05-06 17:15:22 +0700

    update case study image

--- src/components/Explore/index.js
+++ src/components/Explore/index.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 import ArrowRight from '../ArrowRight';
 
-import laptop from '../../assets/images/RefCandy.png';
+import laptop from '../../assets/images/twp.png';
 import './index.sass';
 
 const Explore = () => (
   <section className="explore">
     <div className="explore__card">
       <div className="explore__top-content">
-        <h6>REFERRAL CANDY | FEATURES CASE STUDY</h6>
+        <h6>THE WORK PROJECT | FEATURES CASE STUDY</h6>
         <Link className="explore__case-study" to="/portfolio/referral-candy">
           <h6>VIEW ALL CASE STUDY</h6>
           <ArrowRight className="explore__arrow" />
