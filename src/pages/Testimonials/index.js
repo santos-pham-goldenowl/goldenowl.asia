d069f94139852341dce9593193c524ab9e06ef8c
Austin <austin@goldenowl.asia>
2020-05-12 14:13:55 +0700

    add google-site-verification

--- src/pages/Testimonials/index.js
+++ src/pages/Testimonials/index.js
@@ -31,7 +31,6 @@ const Testimonials = () => {
       <Helmet>
         <title>Testimonials - Golden Owl</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <meta content="N_qR6-efA-BOE-NPwuBG69fmJ-UG_wDHG34i4ixSlug" name="google-site-verification" />
         <meta content="Golden Owl - We do Ruby on Rails, NodeJS, ReactJS and React Native. We follow Agile &amp; TDD practice and cool softwares like Github, Basecamp, Slack in our daily work to provide best communication and transparency to clients. Our services include web development, mobile development, head hunting and more." name="description" /><meta content="Golden Owl - Ruby on Rails, NodeJS, ReactJS and React Native" property="og:title" />
         <meta content="Golden Owl - We do Ruby on Rails, NodeJS, ReactJS and React Native. We follow Agile &amp; TDD practice and cool softwares like Github, Basecamp, Slack in our daily work to provide best communication and transparency to clients. Our services include web development, mobile development, head hunting and more." property="og:description" />
         <meta content={companyLogo} property="og:image" />
