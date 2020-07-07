c5e59d633f76f740cf871089b2026139856d1dc1
edward-le-goldenowl <austin@goldenowl.asia>
2020-07-07 18:00:38 +0700

    add google analytics

--- src/pages/Home/components/Archieves/index.js
+++ src/pages/Home/components/Archieves/index.js
@@ -1,37 +1,37 @@
-import React,{useState, useEffect} from 'react';
-import AnimatedNumber from "animated-number-react";
+import React, { useState, useEffect } from 'react';
+import AnimatedNumber from 'animated-number-react';
 
 import archieves from '../../../../utils/archives';
 import objectToArray from '../../../../utils/objectToArray';
 
 import './index.sass';
-let check =0;
+
+let check = 0;
 const Archieves = () => {
   const [scrolling, setScrolling] = useState(false);
   const [scrollTop, setScrollTop] = useState(0);
   console.log(check);
   useEffect(() => {
-   check=0;
+    check = 0;
   }, []);
   useEffect(() => {
-    const onScroll = e => {
+    const onScroll = (e) => {
       setScrollTop(e.target.documentElement.scrollTop);
-      
-    };  
-    if(scrollTop>2420 && check ===0){
+    };
+    if (scrollTop > 2420 && check === 0) {
       setScrolling(true);
-      check=1;
+      check = 1;
     }
-    window.addEventListener("scroll", onScroll);
-   
-    return () => window.removeEventListener("scroll", onScroll);
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
   }, [scrollTop]);
   return (
     <section className="archieves d-block" id="numanimate">
       <div className="row justify-content-center">
         <p className="archieves__title text-center">
           <strong>Trusted by hundreds of businesses so far</strong>
-          <br/>
+          <br />
           Our clients are from all over the world and are doing amazing things
         </p>
       </div>
@@ -39,35 +39,38 @@ const Archieves = () => {
         {objectToArray(archieves).map((item) => (
           <div className="col-6 col-md-2 d-block archieves__item">
             <center>
-              {scrolling === true ? 
-              <>
-               <h2 transform="rotate(-15 150 150) translate(500,100)">
-               <AnimatedNumber
-                   style={{
-                       transition: '0.8s ease-out',
-                       transitionProperty:
-                           'background-color, color, opacity'
-                   }}
-                   frameStyle={perc => (
-                       perc === 100 ? {} : {opacity: 0.25}
-                   )}
-                   duration= { 3000}
-                   value={item.number}
-                   className="number"
-                   component="text"
-                   formatValue={n => `${Number(n).toFixed(0)}`}/> 
+              {scrolling === true
+                ? (
+                  <>
+                    <h2 transform="rotate(-15 150 150) translate(500,100)">
+                      <AnimatedNumber
+                        style={{
+                          transition: '0.8s ease-out',
+                          transitionProperty:
+                           'background-color, color, opacity',
+                        }}
+                        frameStyle={(perc) => (
+                          perc === 100 ? {} : { opacity: 0.25 }
+                        )}
+                        duration={3000}
+                        value={item.number}
+                        className="number"
+                        component="text"
+                        formatValue={(n) => `${Number(n).toFixed(0)}`}
+                      />
 
-                   <sup>{item.unit}</sup>
-                </h2>
-              <p>{item.key}</p>
-              </>
-              : 
-                <>
-                <h2>{0}</h2>
-                <p>{item.key}</p>
-                </>
-              }
-           
+                      <sup>{item.unit}</sup>
+                    </h2>
+                    <p>{item.key}</p>
+                  </>
+                )
+                : (
+                  <>
+                    <h2>{0}</h2>
+                    <p>{item.key}</p>
+                  </>
+                )}
+
             </center>
           </div>
         ))}
