217e24d5e1efc33d2c7742be899387ecee9b2b95
Austin <austin@goldenowl.asia>
2020-07-22 16:54:07 +0700

    - update item status icon for FAQ page - update services dropdown ui to fix bug card 413 - update testimonial aos for mobile

---
+++ src/pages/FAQ/components/Plus/index.js
@@ -0,0 +1,18 @@
+/* eslint-disable max-len */
+import React from 'react';
+
+const Plus = () => (
+  <svg
+    width={12}
+    height={12}
+    viewBox="0 0 612 612"
+    transform="rotate(45)"
+  >
+    <path
+      d="M612 36.004L576.521.603 306 270.608 35.478.603 0 36.004l270.522 270.007L0 575.997l35.478 35.4L306 341.411l270.521 269.986 35.479-35.4-270.541-269.986z"
+      style={{ stroke: 'black', strokeWidth: '75px' }}
+    />
+  </svg>
+);
+
+export default Plus;
