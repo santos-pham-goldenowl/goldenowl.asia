a3ab6249e46930d757d599c7beac0c4e4601f34c
Edward Le <edward.le.goldenowl@gmail.com>
2020-12-30 16:19:06 +0700

    ui filter categories

---
+++ src/pages/Blog/components/Categories/index.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import arrowDown from '../../../../assets/images/chevron-down-solid.svg';
+import categories from '../../../../utils/blogCategories';
+
+import './index.sass';
+import './categories.sass';
+
+const Categories = () => (
+  <header id="sub-header" className="sub-header">
+    <div className="dropdown">
+      <div className="align-items-center sub-header__button">
+        <Link
+          className="d-flex align-items-center no-hover-text-decoration"
+          onMouseOver={() => document.getElementById('categories-dropdown')
+                && document.getElementById('categories-dropdown').classList.add('dropdown-open')}
+          onMouseLeave={() => document.getElementById('categories-dropdown')
+                && document.getElementById('categories-dropdown').classList.remove('dropdown-open')}
+          onFocus={() => document.getElementById('categories-dropdown')
+                && document.getElementById('categories-dropdown').classList.add('dropdown-open')}
+          onBlur={() => document.getElementById('categories-dropdown')
+                && document.getElementById('categories-dropdown').classList.remove('dropdown-open')}
+        >
+          <h4 className="sub-header__text">All categories</h4>
+          <img className="arrow-down" src={arrowDown} alt="arrow-down" />
+        </Link>
+        <div
+          className="categories-dropdown-top-block"
+          onMouseOver={() => document.getElementById('categories-dropdown')
+                  && document.getElementById('categories-dropdown').classList.add('dropdown-open')}
+          onMouseLeave={() => document.getElementById('categories-dropdown')
+                && document.getElementById('categories-dropdown').classList.remove('dropdown-open')}
+          onFocus={() => document.getElementById('categories-dropdown')
+                && document.getElementById('categories-dropdown').classList.add('dropdown-open')}
+          onBlur={() => document.getElementById('categories-dropdown')
+                && document.getElementById('categories-dropdown').classList.remove('dropdown-open')}
+        />
+      </div>
+      <div id="categories-dropdown" className="dropdown-content">
+        <div
+          className="categories__items"
+          onMouseOver={() => document.getElementById('categories-dropdown')
+                && document.getElementById('categories-dropdown').classList.add('dropdown-open')}
+          onMouseLeave={() => document.getElementById('categories-dropdown')
+                && document.getElementById('categories-dropdown').classList.remove('dropdown-open')}
+          onFocus={() => document.getElementById('categories-dropdown')
+                && document.getElementById('categories-dropdown').classList.add('dropdown-open')}
+          onBlur={() => document.getElementById('categories-dropdown')
+                && document.getElementById('categories-dropdown').classList.remove('dropdown-open')}
+        >
+          {categories.map((category) => (
+            <Link key={category.name}>
+              <div className="categories__category">
+                <div className="categories__category-description-wrapper">
+                  <p>{category.name}</p>
+                </div>
+              </div>
+            </Link>
+
+          ))}
+        </div>
+      </div>
+    </div>
+  </header>
+);
+
+export default Categories;
---
+++ src/pages/Blog/components/Search/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div> Hello world</div>
+    );
+  }
+}
+
+export default Search;
