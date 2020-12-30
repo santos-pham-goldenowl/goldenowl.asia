58d2c2bee4becab29fe7b991bc9e00512a140db5
Edward Le <edward.le.goldenowl@gmail.com>
2020-12-30 17:04:58 +0700

    build ui search box

--- src/pages/Blog/components/Search/index.js
+++ src/pages/Blog/components/Search/index.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 
+import iconSearch from '../../../../assets/images/search.png';
+import './index.sass';
+
 class Search extends Component {
   render() {
     return (
-      <div> Hello world</div>
+      <div className="search__box">
+        <div className="search__icon-wrapper">
+          <img className="icon-search" src={iconSearch} alt="search-icon" />
+        </div>
+        <div className="search__text">
+          <p>Search</p>
+        </div>
+      </div>
     );
   }
 }
