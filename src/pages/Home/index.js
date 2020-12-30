41dc5beae2913c296a6d257ed14cb6f4267258ad
edward-le-goldenowl <65579663+edward-le-goldenowl@users.noreply.github.com>
2020-12-30 09:40:21 +0700

    Revert "Fix/add description text into homepage"

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -26,7 +26,6 @@ const Archieves = Loadable({ loader: () => import('./components/Archieves'), loa
 const Introduction = Loadable({ loader: () => import('./components/Introduction'), loading: Loading, delay: 1300 });
 const Technologies = Loadable({ loader: () => import('./components/Technologies'), loading: Loading, delay: 1300 });
 const CompanyQuality = Loadable({ loader: () => import('./components/CompanyQualities'), loading: Loading, delay: 1300 });
-const IntroVideo = Loadable({ loader: () => import('./components/IntroVideo'), loading: Loading, delay: 1300 });
 const Contact = Loadable({ loader: () => import('../../components/Contact'), loading: Loading, delay: 1300 });
 const Partners = Loadable({ loader: () => import('../../components/Partners'), loading: Loading, delay: 1300 });
 const Footer = Loadable({ loader: () => import('../../components/Footer'), loading: Loading, delay: 1300 });
@@ -56,7 +55,6 @@ const Home = () => {
         <Missions />
         <Introduction />
         <CompanyQuality />
-        <IntroVideo />
         <Archieves />
         <Feedback />
         <hr style={{
