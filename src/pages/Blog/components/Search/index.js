e62d637b2457f836d4076b1adcae192d3c9a2bf3
Edward Le <edward.le.goldenowl@gmail.com>
2020-12-31 12:07:18 +0700

    handle style search box

--- src/pages/Blog/components/Search/index.js
+++ src/pages/Blog/components/Search/index.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import iconSearch from '../../../../assets/images/search.png';
 import './index.sass';
 
-class Search extends Component {
-  render() {
-    return (
-      <div className="search__box">
-        <div className="search__icon-wrapper">
-          <img className="icon-search" src={iconSearch} alt="search-icon" />
-        </div>
-        <div className="search__text">
-          <p>Search</p>
-        </div>
+const Search = () => {
+  const [toggled, setToggle] = useState(false);
+  console.log('🚀 ~ file: index.js ~ line 8 ~ Search ~ toggled', toggled);
+  return (
+    <div className="search__box">
+      {/* <div className="search__icon-wrapper">
+        <img className="icon-search" src={iconSearch} alt="search-icon" />
       </div>
-    );
-  }
-}
+      <div className="search__text">
+        <p>Search</p>
+      </div> */}
+      <img className="icon-search" src={iconSearch} alt="search-icon" placeholder="" />
+      <input onBlur={() => setToggle(false)} onFocus={() => setToggle(true)} className="search-box-blog" />
+    </div>
+  );
+};
 
 export default Search;
